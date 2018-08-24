a6e8b1d3f75518488535d1393243adfb19c3085f
Bhavya Arora <helloworld09071994@gmail.com>
2018-08-24 11:54:49 +0530

    ExpressServer created

--- Express.js
+++ Express.js
@@ -2,27 +2,22 @@ const express=require("express");
 const http=require("http");
 const hostname="localhost";
 const port=3000;
-const app=express();
-const bodyParser=require("body-parser");
-const dishRouter=require("./routes/dishRouter");
 const morgan=require("morgan");
-
-app.use(bodyParser.json());
-
+const server=http.createServer(app);
+const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req,res,next)=>{
-    console.log(req.headers);
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('This is an Express server');
 });
-app.use('/dishes', dishRouter);
 
-app.use(morgan('dev'));
-
-app.use(express.static(__dirname + '/public'));
-
-const server=http.createServer(app);
 
 server.listen(port,hostname,()=>{
   console.log(`server running at http://${hostname}:${port}`)
 });
+// const bodyParser=require("body-parser");
+// const dishRouter=require("./routes/dishRouter");
+// app.use(bodyParser.json());
+// app.use('/dishes', dishRouter);
